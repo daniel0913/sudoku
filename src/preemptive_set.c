7a09b4e82942c9cc7c73f0867ad7da46f38f61b6
Edon Kelmendi <kedon@powell.emi.u-bordeaux1.fr>
2011-10-07 14:14:05 +0200

    (Assignment 3) Styling changes

--- src/preemptive_set.c
+++ src/preemptive_set.c
@@ -4,7 +4,8 @@ const char color_table[] = "123456789"
                            "ABCDEFGHIJKLMNOPQRSTUVXYZ"
                            "abcdefghijklmnopqrstuvxyz"
                            "@&*";
-pset_t char2pset (char c)
+pset_t 
+char2pset (char c)
 {
   for (int i = 0; i < MAX_COLORS; i++)
     {
@@ -14,7 +15,8 @@ pset_t char2pset (char c)
   return ((pset_t) 0);
 }
 
-void pset2str (char string[MAX_COLORS + 1], pset_t pset)
+void 
+pset2str (char string[MAX_COLORS + 1], pset_t pset)
 {
   int j = 0;
   for (int i = 0; i < MAX_COLORS; i++)
@@ -29,20 +31,20 @@ void pset2str (char string[MAX_COLORS + 1], pset_t pset)
   string[j] = '\0';
 }
 
-/*
- * Not sure about this definition
- */
-pset_t pset_full (size_t color_range)
+pset_t 
+pset_full (size_t color_range)
 {
-  return ((pset_t) color_range);
+  return (FULL >> (MAX_COLORS - color_range));
 }
 
-pset_t pset_empty ()
+pset_t 
+pset_empty ()
 {
   return ((pset_t) 0);
 }
 
-pset_t pset_set_or_discard (pset_t pset, char c, bool set)
+pset_t 
+pset_set_or_discard (pset_t pset, char c, bool set)
 {
   pset_t j = 1;
   for (int i = 0; i < MAX_COLORS; i++)
@@ -58,51 +60,59 @@ pset_t pset_set_or_discard (pset_t pset, char c, bool set)
     }
   return (pset);
 }
-pset_t pset_set (pset_t pset, char c)
+pset_t 
+pset_set (pset_t pset, char c)
 {
   return (pset_set_or_discard (pset, c, true));
 }
 
-pset_t pset_discard (pset_t pset, char c)
+pset_t 
+pset_discard (pset_t pset, char c)
 {
   return (pset_set_or_discard (pset, c, false));
 }
 
-pset_t pset_negate (pset_t pset)
+pset_t 
+pset_negate (pset_t pset)
 {
   return (~pset);
 }
 
-pset_t pset_and (pset_t pset1, pset_t pset2)
+pset_t 
+pset_and (pset_t pset1, pset_t pset2)
 {
   return (pset1 & pset2);
 }
 
-pset_t pset_or (pset_t pset1, pset_t pset2)
+pset_t 
+pset_or (pset_t pset1, pset_t pset2)
 {
   return (pset1 | pset2);
 }
 
-pset_t pset_xor (pset_t pset1, pset_t pset2)
+pset_t 
+pset_xor (pset_t pset1, pset_t pset2)
 {
   return (pset1 ^ pset2);
 }
 
-bool pset_is_included (pset_t pset1, pset_t pset2)
+bool 
+pset_is_included (pset_t pset1, pset_t pset2)
 {
   return ((pset1 | pset2) == pset2);
 }
 
 /*
- * can be buggy
+ * might have a bug
  */
-
-bool pset_is_singleton (pset_t pset)
+bool 
+pset_is_singleton (pset_t pset)
 {
   return ((pset & (- pset)) == pset);
 }
 
-size_t pset_cardinality (pset_t pset)
+size_t 
+pset_cardinality (pset_t pset)
 {
   const uint64_t m1  = 0x5555555555555555;
   const uint64_t m2  = 0x3333333333333333;
